8d81dac7d32a44ad6907d2ae300a7c7f8d67538f
Pham Tan Loi <tanloipham05@gmail.com>
2026-03-17 12:06:47 +0700

    Update Dashboard with chart, time line

--- Views/LoginWindow.xaml.cs
+++ Views/LoginWindow.xaml.cs
@@ -1,5 +1,6 @@
-using System.Windows;
 using QuanLyKhachSan_PhamTanLoi.ViewModels;
+using System.Windows;
+using System.Windows.Input;
 
 namespace QuanLyKhachSan_PhamTanLoi.Views;
 
@@ -11,6 +12,32 @@ public partial class LoginWindow : Window
         var vm = new LoginViewModel();
         vm.LoginSuccess += () => DialogResult = true;
         DataContext = vm;
+    }
+
+    // ✅ Enter ở bất kỳ đâu trong Window → login
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+            TriggerLogin();
+    }
+
+    // ✅ Enter trong từng TextBox/PasswordBox
+    private void Input_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            e.Handled = true; // tránh event bubble lên Window
+            TriggerLogin();
+        }
+    }
+
+    private void TriggerLogin()
+    {
+        if (DataContext is LoginViewModel vm &&
+            vm.LoginCommand.CanExecute(PbPassword))
+        {
+            vm.LoginCommand.Execute(PbPassword);
+        }
     }
 
     private void CloseButton_Click(object sender, RoutedEventArgs e)
